b9f360c4e6bed8bae4714967ac63a6a5788ae0bb
Joseph <osekuemen@gmail.com>
2021-06-06 17:48:14 +0100

    Step 7 - Add a ViewModelFactory

---
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModelFactory.kt
@@ -0,0 +1,15 @@
+package com.example.android.guesstheword.screens.score
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class ScoreViewModelFactory(private val finalScore: Int) : ViewModelProvider.Factory {
+    /*Note that the constructor of your view model factory should take any parameters you
+     want to pass into your ScoreViewModel. In this case, it takes in the final score.*/
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ScoreViewModel::class.java)) {
+            return ScoreViewModel(finalScore) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
